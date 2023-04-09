88c0508287008e890767a14281d3ff6399605c2e
SamuelFilgueira <leumas685@gmail.com>
2023-04-09 14:57:46 -0300

    Emotion instalado e fonte padrão aplicada

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Estilos } from "./components/EstilosGlobais/Estilos";
+
+function App() {
+  return (
+    <div className="App">
+      <Estilos />
+      <h1>Frellando</h1> 
+    </div>
+  );
+}
+
+export default App;
